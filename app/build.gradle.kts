0838d82c43d9035101a1f49ca2f78d945484fb11
Him-anshuSharma <sharmanshu0103@gmail.com>
2025-07-07 18:53:38 +0530

    Add maven-publish plugin and publishing config for JitPack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     kotlin("android")
+    id("maven-publish")
 }
 
 android {
@@ -55,4 +56,17 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     testImplementation("org.robolectric:robolectric:4.11.1")
     testImplementation("io.mockk:mockk:1.13.10")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.Him-anshuSharma"
+                artifactId = "apikeymanager"
+                version = "v1.0.0"
+            }
+        }
+    }
 }
